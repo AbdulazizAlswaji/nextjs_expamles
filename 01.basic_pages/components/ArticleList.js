2b45894a78ad772aaf9bba0ecde6305fca1d5bbf
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-04-22 19:58:55 +0300

    exporting out files

--- 01.basic_pages/components/ArticleList.js
+++ 01.basic_pages/components/ArticleList.js
@@ -1,17 +1,14 @@
-import aricleStyles from '../styles/Articles.module.css'
 import ArticleItem from './ArticleItem'
-
+import articleStyles from '../styles/Articles.module.css'
 
 const ArticleList = ({ articles }) => {
-    return (
-        <div className={aricleStyles.grid}>
-            {articles.map((article) =>
-                (
-                    <ArticleItem article={article}/>
-                )
-            )}
-        </div>
-    )
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => (
+        <ArticleItem article={article} key={article.id} />
+      ))}
+    </div>
+  )
 }
 
 export default ArticleList
